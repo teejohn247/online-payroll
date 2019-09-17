97efd2204630b9abf5563690b319187ed9982565
teejohn247 <teejohn247@gmail.com>
2019-09-17 12:53:30 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const axios = require('axios').default;
+
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
